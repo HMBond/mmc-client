b75339d20a4057c81457212f96e7258eaccf4a92
HMBond <hubertusmariabond@gmail.com>
2021-12-11 04:23:53 +0100

    let ViewControl control the active view

--- src/Components/Molecules/Nav/Nav.js
+++ src/Components/Molecules/Nav/Nav.js
@@ -1,18 +1,11 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../../';
+import React from 'react';
 import { ViewControl } from '../../';
 import './Nav.css';
 
 function Nav({ children }) {
-  const { views, setActiveView } = useContext(UserContext);
-
-  function handleViewButtonClick(id) {
-    setActiveView(id);
-  }
-
   return (
     <nav>
-      <ViewControl views={views} onClick={handleViewButtonClick} />
+      <ViewControl />
       {children}
     </nav>
   );
--- src/Components/Molecules/ViewControl/ViewControl.js
+++ src/Components/Molecules/ViewControl/ViewControl.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button } from '@mui/material';
+import { UserContext } from '../../';
 import './ViewControl.css';
 
-function ViewControl({ views, onClick }) {
+function ViewControl() {
+  const { views, activeView, setActiveView } = useContext(UserContext);
+
+  function handleViewButtonClick(id) {
+    setActiveView(id);
+  }
+
   return (
     <div className="view-control">
       {[...views].map(([id, label]) => {
         return (
-          <Button variant="contained" onClick={() => onClick(id)} key={id}>
+          <Button
+            disableElevation={activeView === id}
+            color={activeView === id ? 'warning' : 'primary'}
+            variant="contained"
+            onClick={() => handleViewButtonClick(id)}
+            key={id}
+          >
             {label}
           </Button>
         );
