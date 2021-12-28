30047cf27a3eabe66d83564bb66159b2e34d49ca
HMBond <hubertusmariabond@gmail.com>
2021-12-28 18:08:21 +0100

    clean midiSlider rendering

--- src/Components/Atoms/MidiSlider/MidiSlider.tsx
+++ src/Components/Atoms/MidiSlider/MidiSlider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useContext } from 'react';
+import { useContext } from 'react';
 import { Box, Slider, Typography } from '@mui/material';
 import { MidiContext, UserContext } from '../..';
 import { SliderModule } from '../../../types/modules';
@@ -8,7 +8,7 @@ function MidiSlider(module: SliderModule) {
   const { output } = useContext<any>(MidiContext);
   const { updateModule } = useContext(UserContext) || {};
 
-  function handlePitchbend(event: Event, value: number | number[]) {
+  function handleChange(event: Event, value: number | number[]) {
     output?.channels[channel].sendPitchBend(value);
     if (updateModule)
       updateModule(id, {
@@ -17,27 +17,21 @@ function MidiSlider(module: SliderModule) {
       });
   }
 
-  function RenderSlider(): ReactNode {
-    return (
-      <>
-        {label && <Typography component="label">{label}</Typography>}
-        <Slider
-          disabled={!output}
-          orientation={orientation ? orientation : 'vertical'}
-          value={value}
-          onChange={handlePitchbend}
-          min={-1}
-          max={1}
-          step={0.01}
-        />
-      </>
-    );
-  }
+  const style = orientation === 'horizontal' ? { width: 250 } : { height: 250 };
 
-  return orientation === 'vertical' ? (
-    <Box sx={{ height: 250 }}>{RenderSlider()}</Box>
-  ) : (
-    <Box sx={{ width: 250 }}>{RenderSlider()}</Box>
+  return (
+    <Box sx={style}>
+      {label && <Typography component="label">{label}</Typography>}
+      <Slider
+        disabled={!output}
+        orientation={orientation ? orientation : 'vertical'}
+        value={value}
+        onChange={handleChange}
+        min={-1}
+        max={1}
+        step={0.01}
+      />
+    </Box>
   );
 }
 
