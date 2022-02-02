08fc6e04ddcf6b9e85aa318fe09d6539e93c1bae
HMBond <hubertusmariabond@gmail.com>
2022-02-02 20:35:02 +0100

    :white_check_mark: add filename store test

--- src/__tests__/saveStateAs.test.ts
+++ src/__tests__/saveStateAs.test.ts
@@ -3,11 +3,15 @@ import { reducer } from '../reducers/state.reducer';
 
 describe('save state as', () => {
   global.URL.createObjectURL = jest.fn();
-  test('should save default name when no fileName is given', () => {
+  test('should store the used filename', () => {
+    const newState = reducer(INITIAL_STATE, { type: 'SAVE_STATE_AS', fileName: 'test.txt' });
+    expect(newState.fileName).toBe('test.txt');
+  });
+  test('should store the default name when no fileName is given', () => {
     const newState = reducer(INITIAL_STATE, { type: 'SAVE_STATE_AS', fileName: '' });
     expect(newState.fileName).toBe(DEFAULT_FILE_NAME);
   });
-  test('should always use an extension, even when not given', () => {
+  test('should always use a file extension, even when not given', () => {
     const fileName = 'some-file-name-with-no-extension';
     const newState = reducer(INITIAL_STATE, { type: 'SAVE_STATE_AS', fileName });
     expect(newState.fileName).toBe(fileName + '.json');
