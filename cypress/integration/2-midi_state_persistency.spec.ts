7360ec8585fe317a8a2dbdad24565fd6e23271d4
HMBond <hubertusmariabond@gmail.com>
2022-02-07 03:22:43 +0100

    🧪 pre-test available system midi busses

--- cypress/integration/2-midi_state_persistency.spec.ts
+++ cypress/integration/2-midi_state_persistency.spec.ts
@@ -1,5 +1,23 @@
 /// <reference types="cypress" />
 
+afterEach(function () {
+  // stop running tests if one fails
+  if (this.currentTest.state === 'failed') {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore-next-line
+    Cypress.runner.stop();
+  }
+});
+
+describe('Host system', () => {
+  it('should have at least two available midi busses before running these tests', () => {
+    cy.visitHost();
+    cy.openSettingsDialog();
+    cy.get('select#input-select').find('option').should('have.length.at.least', 2);
+    cy.get('select#output-select').find('option').should('have.length.at.least', 2);
+  });
+});
+
 describe('MIDI state', () => {
   before(() => {
     cy.visitHost();
