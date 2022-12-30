47ffe2e31f1e3da63e8e0af27d77479bdbee7ba2
HMBond <hubertusmariabond@gmail.com>
2022-12-30 08:06:09 +0100

    refactor app envs

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,9 @@ import { connectWebSocket } from './webSocket';
 
 export default function App() {
   const [socket, setSocket] = useState<ReconnectingWebSocket>();
-  const ws = connectWebSocket({
-    host: (import.meta.env.VITE_WS_HOST as string) || 'localhost',
-    port: (import.meta.env.VITE_WS_PORT as string) || '8080',
-  });
+  const host = import.meta.env.VITE_WS_HOST?.toString() || 'localhost';
+  const port = import.meta.env.VITE_WS_PORT?.toString() || '8080';
+  const ws = connectWebSocket({ host, port });
   ws.addEventListener('open', () => {
     setSocket(ws);
   });
