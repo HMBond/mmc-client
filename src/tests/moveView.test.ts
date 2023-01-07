54b6da63ead6710aa7f6c768a8694ec712c6bfb6
HMBond <hubertusmariabond@gmail.com>
2023-01-07 21:50:34 +0100

    remove electron 🗑️

--- src/tests/moveView.test.ts
+++ src/tests/moveView.test.ts
@@ -15,18 +15,22 @@ const mockedState = { ...INITIAL_STATE, views: mockedViews };
 describe('moveView', () => {
   const { views } = reducer(mockedState, { type: 'MOVE_VIEW', view: mockedViews[0], toIndex: 1 });
   const moved = views.find((view) => view.id === mockedViews[0].id);
+
   test('should return a View[] that contains the moved view', () => {
     expect(moved).not.toBeUndefined();
   });
+
   test('should move the view to its new place', () => {
     if (!moved) throw new Error();
     expect(views.indexOf(moved)).toBe(1);
   });
+
   test('should move the next view down when incrementing 1 place up', () => {
     const affected = views.find((view) => view.id === mockedViews[1]?.id);
     if (!affected) throw new Error();
     expect(views.indexOf(affected)).toBe(0);
   });
+
   test('should move all affected views down when incrementing', () => {
     const { views } = reducer(mockedState, { type: 'MOVE_VIEW', view: mockedViews[0], toIndex: 3 });
     expect(views.indexOf(mockedViews[0])).toBe(3);
@@ -34,6 +38,7 @@ describe('moveView', () => {
     expect(views.indexOf(mockedViews[2])).toBe(1);
     expect(views.indexOf(mockedViews[3])).toBe(2);
   });
+
   test('should move all affected views up when decrementing', () => {
     const { views } = reducer(mockedState, { type: 'MOVE_VIEW', view: mockedViews[3], toIndex: 0 });
     expect(views.indexOf(mockedViews[0])).toBe(1);
@@ -41,12 +46,14 @@ describe('moveView', () => {
     expect(views.indexOf(mockedViews[2])).toBe(3);
     expect(views.indexOf(mockedViews[3])).toBe(0);
   });
+
   test('should not affect views that do not need to move', () => {
     const { views } = reducer(mockedState, { type: 'MOVE_VIEW', view: mockedViews[4], toIndex: 3 });
     expect(views.indexOf(mockedViews[0])).toBe(0);
     expect(views.indexOf(mockedViews[1])).toBe(1);
     expect(views.indexOf(mockedViews[2])).toBe(2);
   });
+
   test('should not move view to non existing place', () => {
     expect(() =>
       reducer(mockedState, { type: 'MOVE_VIEW', view: mockedViews[2], toIndex: -1 })
@@ -59,6 +66,7 @@ describe('moveView', () => {
       })
     ).toThrow(Error);
   });
+
   test('should do nothing when index equals destination', () => {
     const { views } = reducer(mockedState, {
       type: 'MOVE_VIEW',
--- src/tests/saveStateAs.test.ts
+++ src/tests/saveStateAs.test.ts
@@ -4,14 +4,17 @@ import { reducer } from '../reducers/state.reducer';
 
 describe('save state as', () => {
   global.URL.createObjectURL = jest.fn();
+
   test('should store the used filename', () => {
     const newState = reducer(INITIAL_STATE, { type: 'SAVE_STATE_AS', fileName: 'test.txt' });
     expect(newState.fileName).toBe('test.txt');
   });
+
   test('should store the default name when no fileName is given', () => {
     const newState = reducer(INITIAL_STATE, { type: 'SAVE_STATE_AS', fileName: '' });
     expect(newState.fileName).toBe(DEFAULT_FILE_NAME + '.json');
   });
+
   test('should always use a file extension, even when not given', () => {
     const fileName = 'some-file-name-with-no-extension';
     const newState = reducer(INITIAL_STATE, { type: 'SAVE_STATE_AS', fileName });
